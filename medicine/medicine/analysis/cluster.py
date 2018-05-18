a44f0fe5f3ec27779d8e6724605988f259bb9fde
Jason <leochung0728@gmail.com>
2018-05-18 15:24:09 +0800

    no message

--- medicine/medicine/analysis/cluster.py
+++ medicine/medicine/analysis/cluster.py
@@ -13,73 +13,92 @@ File_Dir = config.Path['models_dir']
 File_Name = 'kmeans_{}.pkl'
 
 
+def create_model(transform_df, n_clusters=8):
+    model = cluster.KMeans(n_clusters=n_clusters).fit(transform_df)
+    return model
+
+
+def get_data_from_compound():
+    id_list = []
+    name_list = []
+    medicine_list = []
+    for compound in Compound.query.all():
+        id_list.append(compound.id)
+        name_list.append(compound.name)
+
+        medicines = []
+        for medicine in compound.medicine:
+            medicines.append(medicine.name)
+        medicine_list.append(medicines)
+    return id_list, name_list, medicine_list
+
+
+def get_cut_list(datas):
+    cut_list = []
+    for data in datas:
+        data = re.sub('(\s|KT)', '', data)  # 清除 KT 及 空白
+        sentences = re.split('\W', data)  # 依據非字元符號split成句子
+
+        word_list = []
+        for sentence in sentences:
+            word_list.extend([word for word in sentence])  # 1字
+            word_list.extend([sentence[i:i + 2] for i in range(len(sentence) - 1)])  # 2字
+        cut_list.append(word_list)
+    return cut_list
+
+
+def get_transform_df(cut_list):
+    te = TransactionEncoder()
+    te_ary = te.fit(cut_list).transform(cut_list)
+    return pd.DataFrame(te_ary, columns=te.columns_)
+
+
 class Cluster(object):
-
     def __init__(self, n_clusters=8):
-        self.compounds = Compound.query.all()
-        self.compound_names = [c.name for c in self.compounds]
-        self.compound_medicines = self.get_compound_medicines()
-        self.cut_list = self.get_cut_list_by_name()
-        self.df_name_medicine = pd.DataFrame({'medicines': self.compound_medicines,
-                                              'name_cut': self.cut_list},
-                                             index=self.compound_names)
-        self.transform_df = self.get_transform_df()
-        self.dict_id_name = dict(zip(self.transform_df.index, self.compound_names))
-        self.fit = self.get_KMeans_fit(n_clusters)
+        self.compound_df = self.get_compound_df()
+        self.fit = None
+        self.get_KMeans_fit(n_clusters)
 
-    def get_compound_medicines(self):
-        compound_list = []
-        for compound in self.compounds:
-            medicine_list = [medicine.name for medicine in compound.medicine]
-            compound_list.append(medicine_list)
-        return compound_list
+    def get_compound_df(self):
+        compound_df = None
+        df_file_name = 'compound_df'
+        df_file = Path(os.path.join(File_Dir, df_file_name))
 
-    def get_cut_list_by_name(self):
-        cut_list = []
-        for name in self.compound_names:
-            name = re.sub('(\s|KT)', '', name)  # 清除 KT 及 空白
-            sentences = re.split('\W', name)  # 依據非字元符號split成句子
-
-            word_list = []
-            for sentence in sentences:
-                word_list.extend([word for word in sentence])  # 1字
-                word_list.extend([sentence[i:i + 2] for i in range(len(sentence) - 1)])  # 2字
-            cut_list.append(word_list)
-        return cut_list
-
-    def get_transform_df(self):
-        te = TransactionEncoder()
-        te_ary = te.fit(self.cut_list).transform(self.cut_list)
-        return pd.DataFrame(te_ary, columns=te.columns_)
+        if df_file.is_file():
+            compound_df = pd.read_pickle(df_file)
+        else:
+            compound_ids, compound_names, compound_medicines = get_data_from_compound()
+            compound_df = pd.DataFrame(data={'id': compound_ids,
+                                             'name': compound_names,
+                                             'medicine': compound_medicines})
+            compound_df = compound_df.set_index('id')
+            compound_df.to_pickle(df_file)
+        return compound_df
 
     def get_KMeans_fit(self, n_clusters=8):
-        model = None
         model_file = Path(os.path.join(File_Dir, File_Name.format(n_clusters)))
         if model_file.is_file():
-            model = self.load_KMeans_model(model_file)
-            if model.n_clusters != n_clusters:
-                model = None
-        if model is None:
-            model = self.KMeansCluster(n_clusters)
-            self.save_KMeans_model(model, model_file)
-        return model
+            self.load_KMeans_model(model_file)
+            if self.fit.n_clusters != n_clusters:
+                self.fit = None
+        if self.fit is None:
+            compound_names = self.compound_df['name']
+            cut_list = get_cut_list(compound_names)
+            transform_df = get_transform_df(cut_list)
+            self.fit = create_model(transform_df, n_clusters)
+            self.save_KMeans_model(model_file)
 
-    def KMeansCluster(self, n_clusters=8):
-        KMeans = cluster.KMeans(n_clusters=n_clusters)
-        return KMeans.fit(self.transform_df)
-
-    def save_KMeans_model(self, model=None, path=os.path.join(File_Dir, File_Name)):
-        if model is None:
-            model = self.fit
-        joblib.dump(model, path)
+    def save_KMeans_model(self, path=os.path.join(File_Dir, File_Name)):
+        joblib.dump(self.fit, path)
 
     def load_KMeans_model(self, path=os.path.join(File_Dir, File_Name)):
-        return joblib.load(path)
+        self.fit = joblib.load(path)
 
     def get_cluster_dict(self):
-        cluster_dict = {i: np.where(self.fit.labels_ == i)[0] for i in range(self.fit.n_clusters)}
+        cluster_dict = {i: np.where(self.fit.labels_ == i)[0]+1 for i in range(self.fit.n_clusters)}
         return cluster_dict
 
 
 if __name__ == '__main__':
     c = Cluster()
+    print(c.fit)
--- medicine/medicine/views.py
+++ medicine/medicine/views.py
@@ -1,15 +1,16 @@
 from medicine.flask_app import app
 from flask import render_template, request, url_for
-import json
 
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
+
 @app.route('/analysis/apriori', methods=['GET'])
 def apriori():
     return render_template('apriori.html')
+
 
 @app.route('/analysis/apriori/ajax', methods=['GET', 'POST'])
 def apriori_ajax():
@@ -27,31 +28,32 @@ def apriori_ajax():
         apriori.generate_rules(min_support=sup)
     return apriori.rules.to_json(orient='records', force_ascii=False)
 
+
 @app.route('/analysis/cluster', methods=['GET', 'POST'])
 def cluster():
     return render_template('cluster.html')
 
+
 @app.route('/analysis/cluster/ajax', methods=['GET', 'POST'])
 def cluster_ajax():
     import json
-    import pandas as pd
     from collections import Counter
     from medicine.analysis.cluster import Cluster
 
-    kgroup = int(request.form.get('k', 8))
-    group = int(request.form.get('g', 0))
-    c = Cluster(kgroup)
+    k_group = int(request.form.get('k', 8))
+    g_group = int(request.form.get('g', 0))
+
+    c = Cluster(k_group)
+    compound_df = c.compound_df
+    compound_df = compound_df[['name', 'medicine']]
     cluster_dict = c.get_cluster_dict()
-    ids = cluster_dict.get(group)
-    names = [c.dict_id_name[i] for i in ids]
-    medicines = [c.compound_medicines[i] for i in ids]
-    groups = [group] * len(ids)
-    group_df = pd.DataFrame(data={'name': names, 'medicine': medicines, 'group': groups},
-                            columns=['name', 'medicine', 'group'])
+
+    group_df = compound_df.loc[cluster_dict[g_group], ]
+    group_df['group'] = g_group
     table_data = group_df.to_json(orient='values', force_ascii=False)
 
     objs = []
-    for ms in medicines:
+    for ms in group_df['medicine']:
         objs.extend(ms)
     chart_data = sorted(list(Counter(objs).items()), key=lambda tup: tup[1], reverse=True)
 
